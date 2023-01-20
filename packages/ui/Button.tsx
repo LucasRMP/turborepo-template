7ccad4a15e50d335da742d16e4741573bba5d3b6
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2023-01-20 16:52:12 -0300

    feat: change button color to blue

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -5,7 +5,7 @@ export const Button: React.FC = ({ children }) => {
     <button
       type="button"
       role="button"
-      className="bg-green-500 hover:bg-green-700 h-12 text-white font-bold py-2 px-4 rounded transition-all"
+      className="bg-blue-500 hover:bg-blue-700 h-12 text-white font-bold py-2 px-4 rounded transition-all"
     >
       {children}
     </button>
